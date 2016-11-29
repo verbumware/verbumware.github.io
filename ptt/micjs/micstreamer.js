8d7be952419f5cf4d4da0206f2ec46a1a613fef2
alexei-v-ivanov <alexei_v_ivanov@ieee.org>
2016-11-28 21:51:28 -0800

    Update micstreamer.js

--- ptt/micjs/micstreamer.js
+++ ptt/micjs/micstreamer.js
@@ -1573,7 +1573,7 @@ exports.AudioClient = AudioClient;
 
 
 (function(window) {
-  var client = new AudioClient('ws://verbumware.org:8008');
+  var client = new AudioClient('wss://verbumware.org:8008');
   //var client = new AudioClient('ws://127.0.0.1:8008');
   //websocket.onmessage = function (message) {
   //    console.log(message.data);
